6ea6f4d823aabd058754412f70cf6db7f409b36f
Alexander Kumanov <kumana@abv.bg>
2022-02-28 15:28:11 +0200

    added login / logout functionality from the frontend

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+let loginBtn = document.getElementById('login-btn')
+let logoutBtn = document.getElementById('logout-btn')
+
+let token = localStorage.getItem('token')
+
+if (token) {
+    loginBtn.remove()
+} else {
+    logoutBtn.remove()
+}
+
+logoutBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    localStorage.removeItem('token')
+    window.location = 'file:///C:/Users/kumana/Desktop/frontend/devSearch-frontend/login.html'
+})
+
+
 let projectsUrl = 'http://127.0.0.1:8000/api/projects/'
 
 let getProjects = () => {
