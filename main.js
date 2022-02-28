fa0affc00b23135f3fd6cf61ba654c4008042ee4
Alexander Kumanov <kumana@abv.bg>
2022-02-28 12:13:08 +0200

    added post requiest to fronten

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 let projectsUrl = 'http://127.0.0.1:8000/api/projects/'
 
 let getProjects = () => {
@@ -13,6 +11,7 @@ let getProjects = () => {
 
 let buildProjects = (projects) => {
     let projectsWrapper = document.getElementById('projects--wrapper')
+    projectsWrapper.innerHTML = ''
     for (let i=0; i< projects.length; i++) {
         let project = projects[i]
         
@@ -22,16 +21,49 @@ let buildProjects = (projects) => {
                 <div>
                     <div class="card--header">
                         <h3>${project.title}</h3>
-                        <strong class="vote--option">&#43;</strong>
-                        <strong class="vote--option">&#8722;</strong>
+                        <strong class="vote--option" data-vote="up" data-project="${project.id}">&#43;</strong>
+                        <strong class="vote--option" data-vote="down" data-project="${project.id}">&#8722;</strong>
                     </div>
+                    <i>${project.vote_ratio}% Positive feedback </i>
+                    <p>${project.description.substring(0, 150)}</p>
+
                 </div>
                 
             <div>
         `
         projectsWrapper.innerHTML += projectCard
     }
+    addVoteEvents()
 
-}   
+    // Add an event listener
+    
+}
+
+let addVoteEvents = () => {
+    let voteBtns = document.getElementsByClassName('vote--option')
+    let token = localStorage.getItem('token')
+    console.log('TOKEN', token)
+    for (let i=0; i<voteBtns.length; i++) {
+        voteBtns[i].addEventListener('click', (e) => {
+            let vote = e.target.dataset.vote
+            let project = e.target.dataset.project
+
+            fetch(`http://127.0.0.1:8000/api/projects/${project}/vote/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                },
+                body:JSON.stringify({'value': vote})
+                
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success: ', data)
+                getProjects()
+            })
+        })
+    }
+} 
 
 getProjects()
